0670cca62ae8f50e1bd77f15b546cd3272f7bab1
Irell Zane <irellzanedev@gmail.com>
2023-06-11 11:45:09 +0100

    Setup maven and test maven
    Environment configured for compatibility with maven (option 8 required).  Created first variables in getSnailShell function

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,6 +1,7 @@
 package com.premiumminds.internship.snail;
 
 import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by aamado on 05-05-2023.
@@ -10,10 +11,39 @@ class SnailShellPattern implements ISnailShellPattern {
   /**
    * Method to get snailshell pattern
    * 
+   * The solution is as follows:
+   * 
+   * Considering that one lap is from the:
+   * - top row left to right
+   * - right column top to bottom
+   * - bottom row right to left
+   * - left column bottom to top
+   * 
+   * The function adds the values of
+   * the loop to the result and
+   * continue repetitively for the matrix
+   * in the interior of the loop
+   * 
+   * After a certain amount of repetitions
+   * the interior of the loop will be
+   * a 0x0 matrix or a 1x1 matrix whose element
+   * will be added to the result array
+   * 
    * @param matrix matrix of numbers to go through
    * @return order array of values thar represent a snail shell pattern
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
+
+    int rows = matrix.length;
+    int columns = matrix[0].length;
+  
+    int[] result = new int[rows * columns];
+    int resultIndex = 0;
+
+    /* position of current cell to add to the result */
+    int cellRow = 0;
+    int cellColumn = 0;
+
+    return CompletableFuture.completedFuture(result);
   };
 }
